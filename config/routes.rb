d82f294744dbc02fd4459e22b33108f50ef76b19
Estefania <sqr0502@gmail.com>
2015-11-18 09:47:52 -0500

    Create login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get '/login', to: "sessions#new"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

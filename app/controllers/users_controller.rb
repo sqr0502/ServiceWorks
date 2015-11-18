7f9866c35ae30a8dbe99f5d66e4d4e4f39a8c741
Estefania <sqr0502@gmail.com>
2015-11-18 18:37:52 -0500

    create signup form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome #{@user.first_name}! Your account has been succesfully created."
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password,
+                                 :password_confirmation, :street_one, :street_two,
+                                 :state, :zip, :phone, :is_provider, :company_name,
+                                 :company_desc, :business_logo)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
 
   root 'sessions#new'
   get 'sessions/new'
-
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'

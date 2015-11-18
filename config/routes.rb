ef544b9edd4bf23fc42a97088dd67914b34f6682
Estefania <sqr0502@gmail.com>
2015-11-18 14:44:53 -0500

    create flash message for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
   get 'sessions/new'
 
-  get '/login', to: "sessions#new"
-
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

99c2cac3ae9fa89eac8ce1071d01c0399108a646
Chris Morse <Chris@Modake.com>
2015-11-19 18:15:52 -0500

    create quote model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+    
+  has_many :quotes
+  has_many :service_requests
+
 end

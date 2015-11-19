321db54416720a6a2283e636a7147f7e0895b2d0
Chris Morse <Chris@Modake.com>
2015-11-19 17:14:46 -0500

    Save models

--- app/models/service_request.rb
+++ app/models/service_request.rb
@@ -1,2 +1,4 @@
 class ServiceRequest < ActiveRecord::Base
+    has_many :service_request_services
+    has_many :services, through: :service_request_services
 end
--- app/models/service_request_service.rb
+++ app/models/service_request_service.rb
@@ -1,4 +1,4 @@
 class ServiceRequestService < ActiveRecord::Base
-  belongs_to :service_requests
-  belongs_to :services
+  belongs_to :service_request
+  belongs_to :service
 end

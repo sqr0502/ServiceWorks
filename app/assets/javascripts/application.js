0a515304db9f6794447668ff95d918aa9ed2f574
Estefania <sqr0502@gmail.com>
2015-11-19 20:41:01 -0500

    update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require materialize
+//= require materialize-sprockets
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .

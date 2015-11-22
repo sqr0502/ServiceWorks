72e46d0b2b686895ccc24cb9f3478eff225108b9
Chris Morse <Chris@Modake.com>
2015-11-22 12:52:38 -0500

    carrierwave config

--- app/models/service_request.rb
+++ app/models/service_request.rb
@@ -2,5 +2,5 @@ class ServiceRequest < ActiveRecord::Base
     has_many :service_request_services, dependent: :destroy
     has_many :services, through: :service_request_services
     has_many :quotes, dependent: :destroy
-
+    mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151121235212) do
+ActiveRecord::Schema.define(version: 20151122171648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20151121235212) do
     t.datetime "updated_at",       null: false
     t.integer  "user_id"
     t.integer  "users_id"
+    t.string   "image"
     t.string   "status"
   end
 
@@ -72,6 +73,7 @@ ActiveRecord::Schema.define(version: 20151121235212) do
     t.string   "business_logo"
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
+    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree

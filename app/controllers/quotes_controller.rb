094b789cfdb75b015a863bb5db74b2cb16bb5af5
Alex Miller <apmiller108@yahoo.com>
2015-11-24 18:24:08 -0500

    Add code comments to the quotes_controller

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,4 +1,5 @@
 class QuotesController < ApplicationController
+  # NumberHelper is needed to display the currency in the flash message
   include ActionView::Helpers::NumberHelper
   before_action :set_service_request, only: [:new, :create, :accept_quote]
   after_action :set_status, only: [:destroy]
@@ -7,22 +8,30 @@ class QuotesController < ApplicationController
     @quote = current_user.quotes.new
   end
 
+  # Uses can accept a company's quote by clicking the "accept" button/link
   def accept_quote
+    # When the button is clicked, the params contain the quote ID of the accepted quote
     quote = Quote.find(params[:quote_id][:quote_id])
+    # get the company of the accepted quote
     company_name = User.find(quote.user_id).company_name
+    # get the amount of the accepted quote. This value is used in the flash message below
     quote_amount = number_to_currency(quote.amount)
 
+    # Update the quote to status "Accepted"
     quote.update(quote_params)
+    # Update the quote to status "Accepted"
     @service_request.update(service_request_params)
 
-    UserNotifier.new_quote_notification(User.find(quote.user_id)).deliver      
-      
+    # Send email to the company to inform them that the quote has been accepted.
+    UserNotifier.new_quote_notification(User.find(quote.user_id)).deliver
+
     flash[:success] = "You have accepted #{company_name}'s quote for #{quote_amount}"
     redirect_to user_service_request_path(quote.service_request.user_id, quote.service_request.id)
   end
 
   def create
     @quote = current_user.quotes.new(quote_params)
+    # set the quote.user_id to the company ID who created the quote
     @quote.user_id = current_user.id
 
     # creat an array of user_id value from previous quotes, if any.  This array is used in a check to
@@ -52,6 +61,7 @@ class QuotesController < ApplicationController
   end
 
   def destroy
+    # A company can delete a previously created quote as long as it hasn't been accepted
     @quote = Quote.find(params[:id])
     if current_user.id == @quote.user_id
       @quote.destroy
@@ -77,6 +87,8 @@ class QuotesController < ApplicationController
     params.require(:service_request).permit(:additional_notes, :user_id, :status)
   end
 
+  # Set the status ot the service request back to Open if ALL quotes are deleted from a service request
+  # The quote delete link sends a status parameter of "Open"
   def set_status
     service_request = ServiceRequest.find(@quote.service_request_id)
     service_request.update(service_request_params) if service_request.quotes.empty?

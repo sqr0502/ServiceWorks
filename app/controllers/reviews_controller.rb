9be8a9a04fa59f24480d9b775872c45e06ba4962
Alex Miller <apmiller108@yahoo.com>
2015-11-23 20:35:17 -0500

    Flesh out review create method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,12 +4,23 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    @review = Review.new
+    @review = Review.new(review_params)
+
+    if @review.save
+      flash[:sucess] = "Review posted successfully"
+      redirect_to user_service_request.path(@review.user_id, @review.service_request_id)
+    end
   end
 
   def edit
   end
 
   def destroy
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :comment)
   end
 end

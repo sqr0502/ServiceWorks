c073388fe3a39dcfb5de80008a758b7a25ab8bad
Alex Miller <apmiller108@yahoo.com>
2015-11-25 20:05:43 -0500

    Add city to User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151126004404) do
+ActiveRecord::Schema.define(version: 20151126010505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 20151126004404) do
     t.datetime "updated_at",                      null: false
     t.float    "latitude"
     t.float    "logitude"
+    t.string   "city"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree

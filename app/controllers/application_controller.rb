eded6363fe5af08c82d773e59f277a42996daf95
Chris Morse <Chris@Modake.com>
2015-11-27 16:52:38 -0500

    Add twilio SMS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   include ReviewsHelper
-end
-
-
-
-
-services = [ "Kitchen Installation and Repair",'Bathroom Installation and Repair', "Painting Services", "Electrical Services", "HVAC Repair and Installation", "Landscaping Services", "Driveway Installation and Repair",  "Flooring Installation", "Carpentry Services", "Plumbing Services", "Drywall Service", "Power Washing", "Roof Repair", "Fence and Desk Installation and Repair"]
-
-services.each do |s|
-  Service.create(name: s)
+#  include TwilioHelper
 end
